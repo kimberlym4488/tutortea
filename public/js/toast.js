742f6413cb737a5503d38229fd474b0266ba953a
SamusMoran <kimberlym4488@comcast.net>
2022-03-01 00:02:32 -0800

    Updates to styling, typos, and color

--- public/js/toast.js
+++ public/js/toast.js
@@ -9,14 +9,13 @@ Toastify({
   position: "center", // `left`, `center` or `right`
   stopOnFocus: true, // Prevents dismissing of toast on hover
   style: {
-    background: "hsla(129, 18%, 21%, 1)",
+    background: "hsla(168, 18%, 45%, 1)",
 
-    background: "linear-gradient(90deg, hsla(129, 18%, 21%, 0.5) 0%, hsla(120, 25%, 98%, 1) 100%)",
+    background: "linear-gradient(360deg, hsla(168, 18%, 45%, 1) 0%, hsla(129, 18%, 21%, 1) 100%)",
     
-    background: "-moz-linear-gradient(90deg, hsla(129, 18%, 21%, 0.5) 0%, hsla(120, 25%, 98%, 1) 100%)",
+    background: "-moz-linear-gradient(360deg, hsla(168, 18%, 45%, 1) 0%, hsla(129, 18%, 21%, 1) 100%)",
     
-    background: "-webkit-linear-gradient(90deg, hsla(129, 18%, 21%, 0.5) 0%, hsla(120, 25%, 98%, 1) 100%)",
-    
+    background: "-webkit-linear-gradient(360deg, hsla(168, 18%, 45%, 1) 0%, hsla(129, 18%, 21%, 1) 100%)",
   },
 
   onClick: function () {}, // Callback after click
--- seeds/seed.js
+++ seeds/seed.js
@@ -1,10 +1,10 @@
 const sequelize = require("../config/connection");
 const { User, Tutor, Review } = require("../models");
-//needs additional code, await the tutor, review, users seeds files under line 4.
+//imports the user, review, and tutor seed files.
 const userData = require("./userSeeds.json");
 const reviewData = require("./reviewSeeds.json");
 const tutorData = require("./tutorSeeds.json");
-//add review seeds
+
 
 const seedAll = async () => {
   await sequelize.sync({ force: true });
